d5a1aedd7aae1d5953f4a102a29abd1befb95045
ousstarhi <ousstarhi.web@gmail.com>
2019-12-29 18:57:12 +0100

    create playRound function

--- main.js
+++ main.js
@@ -3,4 +3,29 @@ function computerPlay() {
   const choiceNumber = Math.floor(Math.random() * choices.length);
   return choices[choiceNumber];
 }
-Console.log(computerPlay());
+
+function playRound() {
+  const playerRound = prompt('Your turn');
+  if ((playerRound.toLowerCase() !== 'Scissors'.toLocaleLowerCase()) && (playerRound.toLowerCase() !== 'Paper'.toLocaleLowerCase()) && (playerRound.toLowerCase() !== 'Rock'.toLocaleLowerCase())) {
+    alert('You must stick to the rules!');
+    return;
+  }
+  const computerRound = prompt('Computer round', computerPlay());
+  if ((playerRound.toLowerCase() === 'Rock'.toLocaleLowerCase()) && (computerRound === 'Paper')) {
+    alert('You Lose! Paper beats Rock');
+  } else if ((playerRound.toLowerCase() === 'Rock'.toLocaleLowerCase()) && (computerRound === 'Scissors')) {
+    alert('You Won! Rock beats Scissors');
+  } else if ((playerRound.toLowerCase() === 'Paper'.toLocaleLowerCase()) && (computerRound === 'Rock')) {
+    alert('You Won! Paper beats Rock');
+  } else if ((playerRound.toLowerCase() === 'Paper'.toLocaleLowerCase()) && (computerRound === 'Scissors')) {
+    alert('You Lose! Scissors beats Paper');
+  } else if ((playerRound.toLowerCase() === 'Scissors'.toLocaleLowerCase()) && (computerRound === 'Paper')) {
+    alert('You Won! Scissors beats Paper');
+  } else if ((playerRound.toLowerCase() === 'Scissors'.toLocaleLowerCase()) && (computerRound === 'Rock')) {
+    alert('You Lose! Rock beats Scissors');
+  } else {
+    alert('It\'s a draw');
+  }
+}
+
+console.log(playRound());
